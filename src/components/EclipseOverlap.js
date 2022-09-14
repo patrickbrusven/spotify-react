73181f33eb01c910f88a5098fab82b0c1be87671
patrickbrusven <pjbrusven@gmail.com>
2022-09-14 08:53:20 -0700

    eclipse animation with spotify logo

--- src/components/EclipseOverlap.js
+++ src/components/EclipseOverlap.js
@@ -1,9 +1,19 @@
+import SpotifyLogo from "../assets/svgs/SpotifyLogo";
+// import SpotifyLogoFull from "../assets/svgs/SpotifyLogoFull";
+
 const EclipseOverlap = () => {
   return (
     <div className="eclipse-view">
       <div className="eclipse-container">
         <div className="eclipse-container__overlay"></div>
       </div>
+      <div className="logo-eclipse">
+        <div className="bottom-circle"></div>
+        <div className="top-circle">
+          <SpotifyLogo className="spotify-logo"/>
+        </div>
+      </div>
+      {/* <SpotifyLogoFull /> */}
     </div>
   )
 }
