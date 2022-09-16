617c7dbd3e1b949a853fce8dcc4a45e55c99b546
patrickbrusven <pjbrusven@gmail.com>
2022-09-16 14:33:26 -0700

    hover state for playlistCard

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -23,24 +23,29 @@ const PlaylistCard = (
     }
   }, []);
 
+  // const bgImage = {
+  //   backgroundImage : `url(${playlist.images[0].url})`
+  // }
+
   return (
     <li 
       ref={ref} 
-      id={'playlist-' + playlist.id} 
+      id={'playlist-' + playlist.id}
+      className="playlist_card"
+      // style={bgImage} 
       onClick={() => selectPlaylist(playlist.id)}
       onMouseEnter={() => {
-          console.log('hi'); 
           setIsHover(true)
         }
       }
       onMouseLeave={() => setIsHover(false)}
     >
-      { isHover && <div>
+      { isHover && <div className="playlist_card__content">
           <h2>{playlist.name}</h2>
           <p>{playlist.description}</p>
         </div>
       }
-      <img src={playlist.images[0].url} alt={playlist.name}/>
+      <img className={"playlist_card__image" + ( isHover ? " playlist_card__image--hover" : "")} src={playlist.images[0].url} alt={playlist.name}/>
     </li>
   )
 }
