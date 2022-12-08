070ecba52f8ac58a26f578f9c69463a125f65430
patrickbrusven <pjbrusven@gmail.com>
2022-12-08 13:15:09 -0800

    added logo and some copy, styles to support

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,4 +1,5 @@
 import ArrowRight from '../assets/svgs/ArrowRight';
+import SpotifyLogo from '../assets/svgs/SpotifyLogo';
 import { Link, useNavigate } from 'react-router-dom';
 
 function LandingPage() {
@@ -13,15 +14,23 @@ function LandingPage() {
   }
 
   return (
-    <div className="App centered-layout">
-    <h1>Splorify</h1>
-    <Link to="/explore" className="base-anchor" onClick={e => handleTransition(e, '/explore')} >
-      <p>EXPLORE</p><ArrowRight />
-    </Link>
-    <a href={LOGIN_URI} className="base-anchor">
-      <p>LOGIN</p><ArrowRight />
-    </a>
-  </div>
+    <div className="centered-layout">
+      <div className="hero">
+        <div className="hero__heading">
+          <SpotifyLogo />
+          <h1>Splorify</h1> 
+        </div>
+        <h2 className="hero__subheading">Explore the Spotify Web API or login to make the experience more personalized</h2>
+        <div className="hero__buttons">
+          <Link to="/explore" className="base-anchor" onClick={e => handleTransition(e, '/explore')} >
+            <p>EXPLORE</p><ArrowRight />
+          </Link>
+          <a href={LOGIN_URI} className="base-anchor">
+            <p>LOGIN</p><ArrowRight />
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
