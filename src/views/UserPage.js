d68389a686421e34514a057c08b49f7e5dabe9ab
patrickbrusven <pjbrusven@gmail.com>
2022-12-08 21:35:48 -0800

    some user data

--- src/views/UserPage.js
+++ src/views/UserPage.js
@@ -2,6 +2,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import SpotifyConnect from "../components/sections/SpotifyConnect";
 
+const exampleUser = {
+  display_name: '',
+}
+
+const exampleTopArtists = {
+  items: [],
+}
 
 function UserPage() {
   const queryParams = new URLSearchParams(window.location.search);
@@ -9,11 +16,24 @@ function UserPage() {
   const accessToken = queryParams.get('access_token');
   const token_type = queryParams.get('token_type');
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(exampleUser);
+  const [topArtists, setTopArtists] = useState(exampleTopArtists);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios('https://api.spotify.com/v1/me/', {
+        headers: {
+          Authorization: `${token_type} ${accessToken}`
+        },
+      })
+      setUser(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const fetchTopArtists = async () => {
       try {
         const { data } = await axios('https://api.spotify.com/v1/me/top/artists', {
           headers: {
@@ -22,22 +42,40 @@ function UserPage() {
           method: 'GET',
           json: true,
         })
-        setUser(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
+        setTopArtists(data);
+
+      } catch (e) {
+        console.log(e);
       }
-    }
+      
+  }
+
+  useEffect(() => {
+
     fetchUser();
+    fetchTopArtists();
+    setIsLoading(false);
     
   }, [])
 
   return (
     <>
-      {isLoading ?
+      { isLoading ?
         <h1>I'm Loading</h1>
         :
-        <h1>I'm not Loading</h1>
+        <>
+          <h1>Welcome {user.display_name}</h1>
+          <ol>
+            {
+              topArtists.items.map((artist) =>
+                <li>
+                  <img className='playlist_card__image' src={artist.images[2].url} alt={artist.name} /> 
+                  <h3>{artist.name}</h3>
+                </li>
+              )
+            }
+          </ol>
+        </>
       }
       <SpotifyConnect accessToken={accessToken}/>
     </>
