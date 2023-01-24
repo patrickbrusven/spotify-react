487b3c0d3585c772cdbb972d6bc5f2006afef737
patrickbrusven <pjbrusven@gmail.com>
2023-01-24 10:35:35 -0800

    huminize milliseconds updated to include trailing 0

--- src/views/UserPage.js
+++ src/views/UserPage.js
@@ -4,12 +4,22 @@ import SpotifyWebApiPlayer from "../services/api/SpotifyWebApiPlayer";
 import useWebPlaybackState from "../hooks/useWebPlaybackState";
 
 function huminizeMS(ms) {
-  let seconds = Math.floor(ms / 1000);
-  let minutes = Math.floor(seconds / 60);
-  seconds = seconds - minutes * 60;
-  return `${minutes}:${seconds}`;
+  var seconds = (ms / 1000).toFixed(0);
+  var minutes = Math.floor(seconds / 60);
+  var hours = "";
+  if (minutes > 59) {
+    hours = Math.floor(minutes / 60);
+    hours = hours >= 10 ? hours : "0" + hours;
+    minutes = minutes - hours * 60;
+    minutes = minutes >= 10 ? minutes : "0" + minutes;
+  }
+  seconds = Math.floor(seconds % 60);
+  seconds = seconds >= 10 ? seconds : "0" + seconds;
+  if (hours !== "") {
+    return hours + ":" + minutes + ":" + seconds;
+  }
+  return minutes + ":" + seconds;
 }
-
 
 function UserPage() {
   const queryParams = new URLSearchParams(window.location.search);
