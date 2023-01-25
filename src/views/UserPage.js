fb05fd35cec2ae817a31b415eaa4ed99d7119c1a
patrickbrusven <pjbrusven@gmail.com>
2023-01-25 10:37:38 -0800

    fetch audio analysis and pass analysis to AudoBar styled component

--- src/views/UserPage.js
+++ src/views/UserPage.js
@@ -1,8 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
 import SpotifyConnect from "../components/sections/SpotifyConnect";
 import SpotifyWebApiPlayer from "../services/api/SpotifyWebApiPlayer";
+import SpotifyServiceTracks from "../services/api/SpotifyWebApiTracks";
 import useWebPlaybackState from "../hooks/useWebPlaybackState";
 import ProgressBar from "../components/ProgressBar";
+import AudioBar from "../components/StyledAudioBar";
 
 function UserPage() {
   const queryParams = new URLSearchParams(window.location.search);
@@ -48,6 +50,39 @@ function UserPage() {
 
   const [position, duration, progress] = useWebPlaybackState(webPlaybackState);
 
+  // audio analysis
+
+  const audioBarsWrapper = {
+    width: "max-content",
+    height: "50px",
+    backgroundColor: "white",
+    display: "flex",
+    gap: "2px",
+    padding: "0px 2px",
+    alignItems: "flex-end",
+    overflow: "hidden",
+  };
+
+  const [audioAnalysis, setAudioAnalysis] = useState(null);
+  useEffect(() => {
+    const fetchAudioAnalysis = async () => {
+      if (webPlaybackState) {
+        try {
+          const { data } = await SpotifyServiceTracks.getTracksAudioAnalysis(
+            token_type,
+            accessToken,
+            webPlaybackState.track_window.current_track.id
+          );
+          console.log(data);
+          setAudioAnalysis(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    fetchAudioAnalysis();
+  }, [webPlaybackState]);
+
   return (
     <>
       {isLoading ? <h1>I'm Loading</h1> : <h1>I'm not Loading</h1>}
@@ -57,11 +92,42 @@ function UserPage() {
         handleWebPlaybackState={handleWebPlaybackState}
       />
       {webPlaybackState && (
-        <ProgressBar
-          position={position}
-          duration={duration}
-          progress={progress}
-        />
+        <>
+          <ProgressBar
+            position={position}
+            duration={duration}
+            progress={progress}
+          />
+          {audioAnalysis && (
+            <div style={audioBarsWrapper}>
+              <AudioBar
+                position={position}
+                duration={duration}
+                analysis={audioAnalysis.bars}
+              />
+              <AudioBar
+                position={position}
+                duration={duration}
+                analysis={audioAnalysis.beats}
+              />
+              <AudioBar
+                position={position}
+                duration={duration}
+                analysis={audioAnalysis.sections}
+              />
+              <AudioBar
+                position={position}
+                duration={duration}
+                analysis={audioAnalysis.segments}
+              />
+              <AudioBar
+                position={position}
+                duration={duration}
+                analysis={audioAnalysis.tatums}
+              />
+            </div>
+          )}
+        </>
       )}
     </>
   );
